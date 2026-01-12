ae260ddff1c8a69468412ea09ec606391ea0db37
neothabethe <neothabethe2004@gmail.com>
2026-01-12 09:43:24 +0200

    Use env-based API base URL

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5158/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
